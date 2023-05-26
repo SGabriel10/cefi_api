5ba9e5e17d9a51e3aa29a8f230887d39689888c6
SGabriel10 <sergio.gabriel.el10@gmail.com>
2023-05-26 10:31:39 -0400

    discount product stock

--- controllers/saleDetail.js
+++ controllers/saleDetail.js
@@ -1,4 +1,5 @@
 const VentaDetalle = require('../models/VentaDetalle');
+const Producto = require('../models/Producto');
 const obtenerDetalles= async(req,res)=>{
     let detalles= await VentaDetalle.find().populate("producto","descripcion");
 
@@ -11,10 +12,21 @@ const crearDetalle=async (req,res)=>{
     try{
 
         let detalle = new VentaDetalle(req.body);
+        let nuevaCantidad = req.body.detail.product.cantidad- req.body.detail.cantidad
+
         detalle.producto= req.body.detail.product;
         detalle.venta= req.body.venta;
-        detalle.cantidad = req.body.detail.cantidad
+        detalle.cantidad = req.body.detail.cantidad;
 
+        const filter = {
+            descripcion: req.body.detail.product.descripcion
+        }
+        const updateDoc ={
+                $set: {
+                    cantidad: nuevaCantidad
+                }
+        }
+        await Producto.updateOne(filter,updateDoc, {upsert: true});
         await detalle.save();
         return res.status(201).json({
             "ok": true,
@@ -91,5 +103,4 @@ module.exports ={
     crearDetalle,
     actualizarDetalle,
     eliminarDetalle,
-    verificarCantidad
 }
